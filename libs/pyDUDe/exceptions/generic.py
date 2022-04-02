3b47ae7269bb15036b72230253b1e964af83a385
slegrand <ut5svsxgc@relay.firefox.com>
2022-04-01 22:07:57 -0400

    Add generic exceptions

---
+++ libs/pyDUDe/exceptions/generic.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# vim: set ft=python
+#
+# This source file is subject to the Apache License 2.0
+# that is bundled with this package in the file LICENSE.txt.
+# It is also available through the Internet at this address:
+# https://opensource.org/licenses/Apache-2.0
+#
+# @author	Sebastien LEGRAND
+# @license	Apache License 2.0
+#
+# @brief	Generic exceptions
+
+#----- Classes
+class ConnectionError(Exception):
+    """Exception raised for connection issue"""
+
+class UnknownError(Exception):
+    """Exception raised for unknown/unhandled error"""
