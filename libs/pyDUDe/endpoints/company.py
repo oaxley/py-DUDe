5af2bdeb8bbf1f74b153b3dc9126b829acf7e9ec
slegrand <ut5svsxgc@relay.firefox.com>
2022-04-02 15:21:25 -0400

    Add getAllCompanies endpoint

--- libs/pyDUDe/endpoints/company.py
+++ libs/pyDUDe/endpoints/company.py
@@ -13,15 +13,18 @@
 
 #----- Imports
 from __future__ import annotations
-from typing import Any, Dict, Optional
+from typing import Any, Dict, List, Optional, Iterator
+from urllib import response
 
 import requests
 
 from pyDUDe import (
-    Client, exceptions
+    Client, exceptions,
 )
 
-#----- Globals
+from pyDUDe.config import (
+    MAX_SEARCH_LIMIT, DEFAULT_SEARCH_LIMIT
+)
 
 
 #----- Functions
@@ -58,3 +61,53 @@ def createCompany(client: Client, *, name: str) -> int:
     # raise the proper exception depending on the status code
     exception = client.exception(response.status_code)
     raise exception(data['error']['message'])
+
+@Client.endpoint
+def getAllCompanies(client: Client, *, limit = DEFAULT_SEARCH_LIMIT) -> Iterator[Dict[str, Any]]:
+    """Retrieve all the companies
+
+    Args:
+        client: the Client instance
+        offset: the current offset for the search
+        limit: limit the number of records
+
+    Raises:
+        ConnectionError, BadRequest, InternalServerError
+
+    Returns:
+        An iterator to a Company object
+    """
+    offset = 1
+    url = client.url('companies')
+
+    if limit > MAX_SEARCH_LIMIT:
+        limit = MAX_SEARCH_LIMIT
+
+    while True:
+        # prepare the parameters
+        params = {
+            'offset': offset,
+            'limit': limit
+        }
+
+        try:
+            response = requests.get(url, params=params, headers=client.headers(), verify=client.verify(), cert=client.cert())
+            data = response.json()
+
+        except Exception as e:
+            raise exceptions.ConnectionError(e)
+
+        if response.status_code == 200:
+            for item in data['companies']:
+                yield item
+
+            if int(data['count']) < int(data['limit']):
+                break
+            else:
+                offset += int(data['count'])
+
+        else:
+            # raise the proper exception depending on the status code
+            exception = client.exception(response.status_code)
+            raise exception(data['error']['message'])
+
