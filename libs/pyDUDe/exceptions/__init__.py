62da2dda6e40907bc19c216ef7c2599dcb12b4d9
slegrand <ut5svsxgc@relay.firefox.com>
2022-04-01 22:48:08 -0400

    Add more exceptions to package definition

--- libs/pyDUDe/exceptions/__init__.py
+++ libs/pyDUDe/exceptions/__init__.py
@@ -1,1 +1,12 @@
-from .connection import ConnectionError
+from .generic import (
+    ConnectionError, UnknownError
+)
+
+from .http_4xx import (
+    BadRequest,
+    Unauthenticated,
+    Forbidden,
+    NotFound
+)
+
+from .http_5xx import InternalServerError
