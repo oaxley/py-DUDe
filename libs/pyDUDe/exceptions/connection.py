9ba8c1592b4ce370531bea508c4ff9d26765c097
slegrand <ut5svsxgc@relay.firefox.com>
2022-04-01 21:12:32 -0400

    Add basic exception

---
+++ libs/pyDUDe/exceptions/connection.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# vim: set ft=python
+#
+# This source file is subject to the Apache License 2.0
+# that is bundled with this package in the file LICENSE.txt.
+# It is also available through the Internet at this address:
+# https://opensource.org/licenses/Apache-2.0
+#
+# @author	Sebastien LEGRAND
+# @license	Apache License 2.0
+#
+# @brief	Exceptions relative to connection errors
+
+#----- Imports
+
+
+#----- Globals
+
+
+#----- Functions
+
+
+#----- Classes
+class ConnectionError(Exception):
+    pass
